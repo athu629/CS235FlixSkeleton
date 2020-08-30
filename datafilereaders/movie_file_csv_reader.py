a74ec0cb1ec8af3ba7c5dc6e97fa4f74c7a56f7b
athu629 <athu629@aucklanduni.ac.nz>
2020-08-30 15:17:18 +1200

    MovieFileCSVReader class

--- datafilereaders/movie_file_csv_reader.py
+++ datafilereaders/movie_file_csv_reader.py
@@ -9,6 +9,10 @@ class MovieFileCSVReader:
 
     def __init__(self, file_name: str):
         self.__file_name = file_name
+        self._dataset_of_movies = []
+        self._dataset_of_actors = []
+        self._dataset_of_directors = []
+        self._dataset_of_genres = []
 
     def read_csv_file(self):
         with open(self.__file_name, mode='r', encoding='utf-8-sig') as csvfile:
@@ -16,7 +20,59 @@ class MovieFileCSVReader:
 
             index = 0
             for row in movie_file_reader:
-                title = row['Title']
-                release_year = int(row['Year'])
-                print(f"Movie {index} with title: {title}, release year {release_year}")
+                #title = row['Title']
+                #release_year = int(row['Year'])
+                #print(f"Movie {index} with title: {title}, release year {release_year}")
+                #print("HHH", row['Genre'], row['Director'], row['Actors'])
+
+                t = row['Title']
+                y = int(row['Year'])
+                r = int(row['Runtime (Minutes)'])
+                des = row['Description']
+                movie = Movie(t,y)
+                if movie not in self.dataset_of_movies:
+                    movie.runtime_minutes = r
+                    movie.description = des
+                    self.dataset_of_movies.append(movie)
+                
+                actors = row['Actors'].split(",")
+                for a in actors:
+                    act = Actor(a)
+                    movie.add_actor(act)
+                    if act not in self.dataset_of_actors:
+                        self.dataset_of_actors.append(act)
+
+                director = Director(row['Director'])
+                movie.director = director
+                if director not in self.dataset_of_directors:
+                    self.dataset_of_directors.append(director)
+
+                genres = row['Genre'].split(",")
+                for g in genres:
+                    gen = Genre(g)
+                    movie.add_genre(gen)
+                    if gen not in self.dataset_of_genres:
+                        self.dataset_of_genres.append(gen)
+
                 index += 1
+        """
+        print("M", len(self.dataset_of_movies),self.dataset_of_movies)
+        print("A", len(self.dataset_of_actors),self.dataset_of_actors)
+        print("D", len(self.dataset_of_directors),self.dataset_of_directors)
+        print("G", len(self.dataset_of_genres),self.dataset_of_genres)
+        """
+    @property
+    def dataset_of_movies(self) -> list:
+        return self._dataset_of_movies
+
+    @property
+    def dataset_of_actors(self) -> list:
+        return self._dataset_of_actors
+
+    @property
+    def dataset_of_directors(self) -> list:
+        return self._dataset_of_directors
+
+    @property
+    def dataset_of_genres(self) -> list:
+        return self._dataset_of_genres
---
+++ tests/test_movie_file_csv_reader.py
@@ -0,0 +1,11 @@
+from datafilereaders.movie_file_csv_reader import MovieFileCSVReader
+
+def test_init():
+    filename = 'datafiles/Data1000Movies.csv'
+    movie_file_reader = MovieFileCSVReader(filename)
+    movie_file_reader.read_csv_file()
+    assert len(movie_file_reader.dataset_of_movies) == 1000
+    assert len(movie_file_reader.dataset_of_actors) == 1985
+    assert len(movie_file_reader.dataset_of_directors) == 644
+    assert len(movie_file_reader.dataset_of_genres) == 20
+    assert repr(movie_file_reader.dataset_of_movies[0]) == "<Movie Guardians of the Galaxy, 2014>"
