e9c9069ba5369d95a69087f79a2f63a6200b531e
athu629 <athu629@aucklanduni.ac.nz>
2020-09-02 07:52:36 +1200

    Editing watchlist class and corresponding tests

--- domainmodel/watchlist.py
+++ domainmodel/watchlist.py
@@ -1,4 +1,65 @@
 from domainmodel.movie import Movie
 
 class WatchList:
-    pass
+
+    def __init__(self):
+        self._watch_list = []
+        self._ptr = 0
+
+    def add_movie(self, movie: Movie):
+        if type(movie) is Movie and movie not in self._watch_list:
+            self._watch_list.append(movie)
+
+    def remove_movie(self, movie: Movie):
+        if type(movie) is Movie and movie in self._watch_list:
+            self._watch_list.remove(movie)
+
+    def __repr__(self):
+        if len(self._watch_list) == 0:
+            return f"You have no movies saved in your watchlist."
+        else:
+            string = "Watchlist:\n"
+            for x in range(len(self._watch_list)):
+                if x == len(self._watch_list) - 1:
+                    y = str(x + 1) + " : " + str(self._watch_list[x])
+                else:
+                    y = str(x+1) + " : " + str(self._watch_list[x]) + "\n"
+                string = string + y
+            return f"{string}"
+
+    def __eq__(self, other):
+        l1 = self._watch_list
+        l1.sort()
+        l2 = other._watch_list
+        l2.sort()
+        return l1 == l2
+
+    def select_movie_to_watch(self, index):
+        if type(index) is int and 0 <= index < len(self._watch_list):
+            return self._watch_list[index]
+        elif index < 0 or index >= len(self._watch_list):
+            return None
+
+    def size(self):
+        return len(self._watch_list)
+
+    def first_movie_in_watchlist(self):
+        if len(self._watch_list) > 0:
+            return self._watch_list[0]
+        elif len(self._watch_list) == 0:
+            return None
+
+    def __iter__(self):
+        self._ptr = 0
+        return self
+
+    def __next__(self):
+        if self._ptr == len(self._watch_list):
+            raise StopIteration
+        s = self._watch_list[self._ptr]
+        self._ptr += 1
+        return s
+
+    @property
+    def watch_list(self):
+        return self._watch_list
---
+++ tests/test_watchlist.py
@@ -0,0 +1,70 @@
+import pytest
+
+from domainmodel.watchlist import WatchList
+from domainmodel.movie import Movie
+from domainmodel.review import Review
+
+def test_init():
+    w1 = WatchList()
+    assert len(w1.watch_list) == 0
+
+def test_add_movie():
+    w1 = WatchList()
+    w1.add_movie(Movie("Moana", 2016))
+    assert len(w1.watch_list) == 1
+
+def test_remove_movie():
+    w1 = WatchList()
+    w1.add_movie(Movie("Moana", 2016))
+    w1.add_movie(Movie("Ice Age", 2002))
+    assert len(w1.watch_list) == 2
+    w1.remove_movie(Movie("Moana", 2016))
+    assert repr(w1.watch_list) == "[<Movie Ice Age, 2002>]"
+
+def test_repr():
+    w1 = WatchList()
+    assert w1.__repr__() == "You have no movies saved in your watchlist."
+    w1.add_movie(Movie("Moana", 2016))
+    w1.add_movie(Movie("Ice Age", 2002))
+    assert w1.__repr__() == "Watchlist:\n"\
+                            "1 : <Movie Moana, 2016>\n"\
+                            "2 : <Movie Ice Age, 2002>"
+
+def test_eq():
+    w1 = WatchList()
+    w1.add_movie(Movie("Moana", 2016))
+    w1.add_movie(Movie("Ice Age", 2002))
+    w2 = WatchList()
+    w2.add_movie(Movie("Ice Age", 2002))
+    w2.add_movie(Movie("Moana", 2016))
+    assert w1.__eq__(w2) is True
+    w3 = WatchList()
+    w3.add_movie(Movie("House Bunny", 2002))
+    assert w1.__eq__(w3) is False
+
+def test_select_movie_to_watch():
+    pass
+
+def test_size():
+    w1 = WatchList()
+    assert w1.size() == 0
+    w1.add_movie(Movie("Moana", 2016))
+    w1.add_movie(Movie("Ice Age", 2002))
+    assert w1.size() == 2
+
+def test_first_movie_in_watchlist():
+    w1 = WatchList()
+    assert w1.first_movie_in_watchlist() is None
+    w1.add_movie(Movie("Moana", 2016))
+    assert repr(w1.first_movie_in_watchlist()) == "<Movie Moana, 2016>"
+
+def test_iter_and_next():
+    w1 = WatchList()
+    w1.add_movie(Movie("Moana", 2016))
+    w1.add_movie(Movie("Ice Age", 2002))
+    iter_lst = w1.__iter__()
+    with pytest.raises(StopIteration):
+        while True:
+            iter_lst.__next__()
+
+
